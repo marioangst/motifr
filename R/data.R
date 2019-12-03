e04ac15b0133f22189054b50a8972065c675e82a
Mario Angst <mario.angst@eawag.ch>
2019-12-03 18:26:12 +0100

    added aggregate_cld function

---
+++ R/data.R
@@ -0,0 +1,65 @@
+#' Actor-concept edgelist
+#'
+#' A dataset
+#'
+#' @format A data frame with n rows and n variables:
+#' \describe{
+#'   \item{sender}{tbd}
+#'   \item{receiver}{tbd}
+#'   ...
+#' }
+#' @source survey
+"actor_concept_el"
+
+#' Actor dataframe
+#'
+#' A dataset
+#'
+#' @format A data frame with n rows and n variables:
+#' \describe{
+#'   \item{actor}{tbd}
+#'   ...
+#' }
+#' @source survey
+"actors"
+
+#' Actor edgelist
+#'
+#' A dataset
+#'
+#' @format A data frame with n rows and n variables:
+#' \describe{
+#'   \item{sender}{tbd}
+#'   \item{receiver}{tbd}
+#'   ...
+#' }
+#' @source survey
+"actor_el"
+
+#' CLD concepts dataframe
+#'
+#' A dataset
+#'
+#' @format A data frame with n rows and n variables:
+#' \describe{
+#'   \item{concept}{tbd}
+#'   \item{type}{tbd}
+#'   \item{english}{tbd}
+#'   ...
+#' }
+#' @source survey
+"cld_concepts"
+
+#' CLD edgelist dataframe
+#'
+#' A dataset
+#'
+#' @format A data frame with n rows and n variables:
+#' \describe{
+#'   \item{sender}{tbd}
+#'   \item{receiver}{tbd}
+#'   \item{sign}{tbd}
+#'   ...
+#' }
+#' @source survey
+"cld_el"
