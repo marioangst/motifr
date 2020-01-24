753223e92b2657ef0fb06049967f605da88b07d5
Mario Angst <mario.angst@gmail.com>
2020-01-24 13:43:32 +0100

    added python sma package integration (I think)

--- R/count_motifs.R
+++ R/count_motifs.R
@@ -1,16 +1,48 @@
 
+#' Translate multi-level statnet network object to networkx (python) object
+#'
+#' @param g statnet network object
+#' @param typeAttr character vector specifying the attribute name where level information is stored in statnet object
+#' @param relabel should nodes be relabeled with statnet vertex.names? (defaults to TRUE)
+#'
+#' @return python graph object
+#' @export
+#'
+#' @examples
+toPyGraph <- function(g, typeAttr, relabel = TRUE) {
+  # function for translating a statnet network object into a Python compatible
+  # networkx object
+  adjacencyMatrix = network::as.matrix.network(g)
+  attributeNames  = network::list.vertex.attributes(g)
+  attributeValues = lapply(network::list.vertex.attributes(g),
+                           function(x) network::get.vertex.attribute(g,x))
+  py_g <- sma$translateGraph(adjacencyMatrix, attributeNames, attributeValues, typeAttr)
 
-
-# this accesses python functions in the sesmotifs submodule. see eg. - I chose Version 1 here because
-# sesmotifanalyzer functions do not need to be accessible at top-level
-# https://community.rstudio.com/t/build-package-namespace-dynamically-during-onload/4101
-# https://mran.microsoft.com/snapshot/2017-08-06/web/packages/reticulate/vignettes/package.html
-
-sma <- NULL
-.onLoad <- function(libname, pkgname){
-  sma <<- reticulate::import_from_path(module = 'sma',
-                                       path = system.file("python",
-                                                          'sesmotifanalyser',
-                                                          package = packageName(),
-                                                          mustWork = TRUE))
+  if (relabel == TRUE){
+    # JS: renaming in here right now, but will suggest update to rbridge.py to do in Python
+    node_names <-
+      reticulate::py_dict(keys = as.integer(0:(py_g$number_of_nodes()-1)),
+              values = network::get.vertex.attribute(g, "vertex.names"))
+    py_g <- nx$relabel_nodes(py_g,mapping = node_names)
+  }
+  py_g
 }
+
+
+# load("data/reussebene_mlnet.RData")
+# # get python equivalent to statnet network type
+# pyGraph = toPyGraph(ml_net, 'sesType')
+#
+# nx$nodes(pyGraph)
+#
+# sma$ThreeEMotifs(G = pyGraph)
+#
+# # count motifs
+#
+# # first create set specifying types of motifs (3 or 4 (or any?)), then type eg. II.B
+# # then count motifs
+# # achtung:
+# # procedure empties "set" object after iteration, thus always needs to be rewritten (not very R-like)
+#
+# set_3_IIC <- sma$motifSet(sma$ThreeEMotifs(pyGraph), sma$is3Type("II.C"))
+# sma$countAnyMotifs(set_3_IIC)
---
+++ R/load_python.R
@@ -0,0 +1,15 @@
+# this accesses python functions in the sesmotifs submodule. see eg. - I chose Version 1 here because
+# sesmotifanalyzer functions do not need to be accessible at top-level
+# https://community.rstudio.com/t/build-package-namespace-dynamically-during-onload/4101
+# https://mran.microsoft.com/snapshot/2017-08-06/web/packages/reticulate/vignettes/package.html
+
+sma <<- reticulate::import_from_path(module = "sma", path = system.file("python",
+                                                                        "sesmotifanalyser",
+                                                                        package = packageName()))
+
+nx <- NULL
+
+.onLoad <- function(libname, pkgname) {
+  # delay load nx module (will only be loaded when accessed via $)
+  nx <<- reticulate::import("networkx", delay_load = TRUE)
+}
