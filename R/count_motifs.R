279a840619b28afd2ff8a9d4040d2bbdb38ded11
Mario Angst <mario.angst@gmail.com>
2020-02-03 17:22:14 +0100

    first version of count_motifs

--- R/count_motifs.R
+++ R/count_motifs.R
@@ -77,4 +77,57 @@ show_4_motifs <- function(){
 # set_3_IIC <- sma$motifSet(sma$ThreeEMotifs(pyGraph), sma$is3Type("II.C"))
 # sma$countAnyMotifs(set_3_IIC)
 
+#' Count multi-level motifs
+#'
+#' @param net A statnet network object with a node attribute specifying the level of each node
+#' @param type_attr character vector specifying the attribute name where level information is stored in statnet object
+#' @param number_nodes The number of nodes in the motif to be counted (3 or 4 are possible)
+#' @param motif The motif name. Use show_3_motifs() or show_4_motifs() to see possible motifs
+#'
+#' @return Integer giving the motif count
+#' @export
+#'
+#' @examples
+count_motifs <- function(net, type_attr, number_nodes, motif){
+
+  if(!(number_nodes %in% c(3,4))){
+    stop(paste("At the moment, only motifs with 3 or 4 nodes can
+         be classified. You entered",number_nodes,".
+               Use show_3_motifs() or show_4_motifs() to see possible motifs"))
+  }
+
+  all_possibilities_4 <- expand.grid(
+    c("I","II","III","IV","V","VI","VII"),
+    c("A","B","C","D"))
+  all_possibilities_4 <- paste(all_possibilities_4$Var1,all_possibilities_4$Var2,sep = ".")
+
+  all_possibilities_3 <- expand.grid(
+    c("I","II"),
+    c("A","B","C","D"))
+  all_possibilities_3 <- paste(all_possibilities_3$Var1,all_possibilities_3$Var2,sep = ".")
+
+  if(number_nodes == 3 & !(motif %in% all_possibilities_3)){
+    stop(paste("This motif (",
+               motif,
+                ") cannot be counted. For three nodes, only these are possible: ",
+               paste(all_possibilities_3,collapse = ", ")))
+  }
+
+  if(number_nodes == 4 & !(motif %in% all_possibilities_4)){
+    stop(paste("This motif (",
+               motif,
+                ") cannot be counted. For four nodes, only these are possible: ",
+               paste(all_possibilities_4,collapse = ", ")))
+  }
+
+  py_g <- integrateR::toPyGraph(net,typeAttr = type_attr)
+  if(number_nodes == 3){
+    motif_set <- sma$motifSet(sma$ThreeEMotifs(py_g), sma$is3Type(motif))
+    sma$countAnyMotifs(motif_set)
+  }
+  if(number_nodes == 4){
+    motif_set <- sma$motifSet(sma$FourMotifs(py_g), sma$is4Type(motif))
+    sma$countAnyMotifs(motif_set)
+  }
+}
 
