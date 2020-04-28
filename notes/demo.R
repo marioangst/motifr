47c9f2e00e297348d148f65c3fb523e207bf97f1
Mario Angst <mario.angst@gmail.com>
2020-04-28 16:05:51 +0200

    some example outputs in notes

--- notes/demo.R
+++ notes/demo.R
@@ -6,6 +6,8 @@ library(motifr)
 motifs = list('1,2[I.C]', '1,2[II.C]')
 motif = c('1,2[II.C]') # closed triangle
 
+count_motifs(ml_net, '1,2[II.C]')
+
 count_motifs(ml_net, motifs = motifs, lvl_attr = "sesType")
 motifs_distribution(ml_net, motifs = motifs)
 motif_summary(ml_net)
@@ -17,11 +19,11 @@ simulate_baseline(ml_net, motifs = motifs, n = 20)
 plot_mnet(net = dummy_net,lvl_attr = "sesType")
 
 compare_to_baseline(ml_net, motifs = motifs, n = 10)
-compare_to_baseline(ml_net, motifs = motifs, n = 100)
+compare_to_baseline(ml_net, motifs = motifs, n = 500)
 
 compare_to_baseline(dummy_net, motifs = list('1,2[I.C]', '1,2[II.C]'))
 
 identify_gaps(ml_net, motif = motif)
 critical_dyads(ml_net, motif = motif)
 
-plot_gaps(ml_net, motif = motif, label = TRUE, cutoff = 3)
+plot_gaps(ml_net, motif = motif, label = TRUE, cutoff = 5, subset_graph = "partial")
